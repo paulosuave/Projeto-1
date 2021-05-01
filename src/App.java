a10c1500cadaf5164414adf1322331ce4195e2d7
paulosuave <83079255+paulosuave@users.noreply.github.com>
2021-05-01 11:01:09 -0300

    teste inicial

---
+++ src/App.java
@@ -0,0 +1,20 @@
+package com.h2;
+
+/**
+ * lembre-se que você começou esse programa para ajudar os alunos a entender lógica de programação
+ * e programação de computadores!
+ */
+
+public class App{
+    public static void main (String [] args) {System.out.println("Olá Mundo");}
+
+    public static int doubleTheNumber(int number) {return 2 * number;}
+
+    private static int add(int[] numbers) {
+        var sum = 0;
+        for(int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+}
